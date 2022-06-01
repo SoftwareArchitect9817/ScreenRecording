2582591d1de9e9f9cf53c6acf772d830832b6c6b
Derek Jones <104667003+SoftwareArchitect9817@users.noreply.github.com>
2022-06-01 12:26:50 +0200

    first commit

---
+++ ScreenRecording/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ScreenRecording
+//
+//  Created by Zhang Xiqian on 2020/7/29.
+//  Copyright © 2020 RUBY MAE Brown. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
